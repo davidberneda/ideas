a43ed93ce9fa15c9b4762103d2cc475e897a8939
David Berneda <davidberneda@gmail.com>
2021-03-24 15:18:59 +0100

    Update index.js

--- maps/index.js
+++ maps/index.js
@@ -1,7 +1,9 @@
 function initMap() {
-  const babili = { lat: 32.536389, lng: 44.420833 };
+
+  const babili_pos = { lat: 32.536389, lng: 44.420833 };
+  const map = new google.maps.Map(document.getElementById("map"), { zoom: 7, center: babili_pos, });
   
-  const map = new google.maps.Map(document.getElementById("map"), { zoom: 4, center: babili, });
+  const agade = new google.maps.Marker({ title: "Agade / Tell Mohammed", position: { lat: 36.924371, lng: 41.564096 }, map: map, });
+  const babili = new google.maps.Marker({ title: "Babili", position: babili_pos, map: map, });
   
-  const marker = new google.maps.Marker({ position: babili, map: map, });
 }
