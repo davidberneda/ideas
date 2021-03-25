6e82e350e142e09eef0b5f115b997be644e7757f
david <david@steema.com>
2021-03-25 11:01:04 +0100

    no message

--- maps/index.js
+++ maps/index.js
@@ -6,22 +6,22 @@ function initMap() {
 
   const map = new google.maps.Map(document.getElementById("map"), { zoom: 8, center: babili_pos, });
 
-  const agade = new google.maps.Marker({ title: "Agade / Tell Mohammed",
+  const agade = new google.maps.Marker({ title: "Agade / Tell Mohammed", label: "Agade",
                      position: { lat: 33.90962758243391, lng: 44.53480309997145 }, map: map, });
 
-  const babili = new google.maps.Marker({ title: "Babili / Babylon",
+  const babili = new google.maps.Marker({ title: "Babili / Babylon", label: "Babili",
                      position: babili_pos, map: map, });
 
-  const barsipa = new google.maps.Marker({ title: "Barsipa / Birs Nimrud",
+  const barsipa = new google.maps.Marker({ title: "Barsipa / Birs Nimrud", label: "Barsipa",
                      position: { lat: 32.39198758683342, lng: 44.34127626903373 }, map: map, });
 
-  const emeslam = new google.maps.Marker({ title: "Emeslam / Nergal, Cutha",
+  const emeslam = new google.maps.Marker({ title: "Emeslam / Nergal, Cutha", label: "Emeslam",
                      position: { lat: 32.760028, lng: 44.612861 }, map: map, });
 
-  const eridu = new google.maps.Marker({ title: "Eridu / Abu Shahrein",
+  const eridu = new google.maps.Marker({ title: "Eridu / Abu Shahrein", label: "Eridu",
                      position: { lat: 30.82616123644755, lng: 45.99465066921363 }, map: map, });
 
-  const esarra = new google.maps.Marker({ title: "Ešarra / Nippur",
+  const esarra = new google.maps.Marker({ title: "Ešarra / Nippur", label: "Ešarra",
                      position: { lat: 32.1261, lng: 45.2308 }, map: map,
                      icon: image });
 
